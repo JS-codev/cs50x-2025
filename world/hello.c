243b300dc6e5ba4b23552e22eee6e59c2a630310
Joshua Soh <lone7walker@gmail.com>
2025-07-28 11:01:11 +0000

    Mon, Jul 28, 2025, 7:01 PM +08:00

--- world/hello.c
+++ world/hello.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
 int main() {
-        string name = get_string("what is your name?");
-        scanf("%s", age);
+    string name = get_string("what is your name?");
     printf("hello, %s\n", name);
-    printf("Your age is:", age, "years old");
 }

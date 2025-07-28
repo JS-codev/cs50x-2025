9a1214a7a55d1fa5130a2a6f9e249b2e640a8799
Joshua Soh <lone7walker@gmail.com>
2025-07-28 12:11:25 +0000

    Mon, Jul 28, 2025, 8:11 PM +08:00

--- Mario-more/mario.c
+++ Mario-more/mario.c
@@ -1,27 +1,31 @@
+#include <cs50.h>
 #include <stdio.h>
-#include "cs50.h"
 
 void print_row(int bricks);
 
-int main(void) {
-    int n; // get no. of bricks
+int main(void)
+{
+    // Prompt the user for the pyramid's height
+    int n; 
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1);
 
-    // input
-    do {
-        get_int("Int: ");
-    } while (n < 0); // keep asking for a positive input always
-
-    // Print
-    for (int i = 0; i < n; i++) {
-        print_row(n-1);
+    // Print a pyramid of that height
+    for (int i = 0; i < n; i++)
+    {
+        // Print row of bricks
+        print_row(i + 1);
     }
 }
 
-void print_row(int n) {
-    for (int a = 0; a < n; a++) {
-        printf(" "); // print empty space
+void print_row(int bricks)
+{
+    for (int i = 0; i < bricks; i++)
+    {
+        printf("#");
     }
-    printf("#"); // after reaching end it prints #
-
-    printf("\n"); // go to next line
+    printf("\n");
 }

23acbd0a1cece18fe91e336fe4e9ec023ae42d55
Joshua Soh <lone7walker@gmail.com>
2025-07-28 12:50:52 +0000

    Mon, Jul 28, 2025, 8:50 PM +08:00

--- Mario-more/mario.c
+++ Mario-more/mario.c
@@ -1,7 +1,7 @@
-#include <cs50.h>
+
 #include <stdio.h>
 
-void print_row(int bricks);
+void print_row(int spaces, int bricks);
 
 int main(void)
 {
@@ -9,25 +9,41 @@ int main(void)
     int n;
     do
     {
-        n = get_int("Height: ");
+        get_int("Height:");
     }
-    while (n < 1);
+    while (n < 1 && n > 8); // prompts them only for 1 - 8 int inputs.
 
-    // Print a pyramid of that height
-    for (int i = 0; i < n; i++)
+    // Print a pyramid of that height (e.g user input 5)
+    for (int i = 0; i < n; i++) // i becomes 1 after 1st iteration.
     {
         // Print row of bricks
-        print_row(i + 1);// i becomes 1, i becomes 2,
+        int spaces = n - i - 1; // 1st iteration: 5 - 0 - 1 = 4
+        int bricks = i + 1;    // 1st iteration: 0 + 1 = 1
+        print_row(spaces, bricks);
     }
-    }
+    return 0;
 }
 
-void print_row(int bricks)
-{
-    for (int i = 0; i < bricks; i++) // bricks = 1 but i remains 0, brickes becomes 2 but i remains 0.
-{
-    {
-        printf("#");
+
+void print_row(int spaces, int bricks) { // 1st iteration: (4, 1)
+    for (int i = 0; i < spaces; i++) {
+        printf(" "); // 1st iterate: print 4 spaces first
     }
-    printf("\n");
+
+    for (int i = 0; i < bricks; i++)
+    {
+        printf("#"); // 1st iterate: print a # at 5th position.
+    }
+
+    // print 2 empty lines (in middle hill):
+    printf(" ");
+    printf(" ");
+
+
+    for (int i = 0; i < bricks; i++) // Go to next right-side of hill
+    {
+        printf("#"); // 1st iterate: print a #.
+    }
+
+    printf("\n"); // go to next line
 }

f3c9fe9804253426a223f458c845a06e2c773b0e
Joshua Soh <lone7walker@gmail.com>
2025-08-03 05:39:41 +0000

    Sun, Aug 3, 2025, 1:39 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -22,6 +22,14 @@ node *table[N];
 bool check(const char *word)
 {
     // TODO
+    int index = hash(word);
+    node *cursor = table[index];
+    while (cursor != NULL)
+    {
+        if (strcasecmp(cursor->word, word) == 0)
+            return true;
+        cursor = cursor->next;
+    }
     return false;
 }
 

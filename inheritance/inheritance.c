6358157e4f5dd33daffccd30379cb3c643279621
Joshua Soh <lone7walker@gmail.com>
2025-08-03 05:22:50 +0000

    Sun, Aug 3, 2025, 1:22 PM +08:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -49,8 +49,8 @@ person *create_family(int generations)
     if (generations > 1)
     {
         // Create two new parents for current person by recursively calling create_family
-        person *parent0 = create_family(generations - 1);
-        person *parent1 = create_family(generations - 1);
+        p->parents[0] = create_family(generations - 1);
+        p->parents[1] = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
         p->alleles[0] = p->parents[0]->alleles[rand() % 2];
@@ -62,8 +62,8 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-        p->parents[0] = p->parents[1] = NULL;
-
+        p->parents[0] = NULL;
+        p->parents[1] = NULL;
         // TODO: Randomly assign alleles
         p->alleles[0] = random_allele();
         p->alleles[1] = random_allele();
